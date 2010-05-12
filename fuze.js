59710bf35483cb1719dd1ac4303d7a1cde2b52da
Senmiao Liu <zir.echo@gmail.com>
2010-05-12 17:43:21 +0800

    replace old implementation, better error handling

--- fuze.js
+++ fuze.js
@@ -5,43 +5,33 @@
 
 
 (function() {
+    var slice = Array.prototype.slice;
+    var EventEmitter = require('events').EventEmitter;
     var Fuze = function() {
         this.emitter = new EventEmitter;
     };
-    var slice = Array.prototype.slice;
-    var EventEmitter = require('events').EventEmitter;
 
     Fuze.prototype = {
-        eos: function(err) {
-            if (err) 
-                this.emit('err', err)
-            else
-                this.apply(this, slice.call(arguments, 1));
-        },
-
-        defEos: function(fn) {
-            this.eos = fn;
-        },
-
-        onError: function(callback) {
+        onError: function(callback, clear) {
+            clear && this.emitter.removeAllListeners('err');
             this.emitter.addListener('err', callback);
         },
 
-        chain2: function() {
-            var chain = slice.call(arguments);
-            var fuze = this;
-            var step = 0;
+        chain: function() {
+            var chain = slice.call(arguments), fuze = this, step = 0, emitter = new EventEmitter;
             function next() {
                 if (chain.length <= 0) {
                     return;
                 }
                 step++; // where we stay
-                var link = chain.shift();
+                var link = chain.shift(), func, funcThis, funcArgs;
                 if (typeof link == 'function') {
                     // link is a function, just call
-                    link.apply(next, arguments);
+                    func = link;
+                    funcThis = next;
+                    funcArgs = arguments;
                 } else if (link instanceof Array) {
-                    var args = slice.call(arguments), funcThis;
+                    var args = slice.call(arguments);
                     if (typeof link[0] === 'number') {
                         // the index of the argument, which should be used as funcThis
                         funcThis = args[link.shift()];
@@ -50,7 +40,7 @@
                         // call function by name
                         // assume 'this' of the function is the first argument if not set yet
                         funcThis = funcThis || args[0];
-                        var func = funcThis[link[0]];
+                        func = funcThis[link[0]];
                         if (typeof func !== 'function')
                             throw new Error('function not exists or it\'s not a function');
                     } else {
@@ -63,47 +53,47 @@
                         // use default error handling method provided by Fuze
                         link.push(next.eos);
                     }
-                    if (func.length == link.length - 1) {
-                        // we have all we need to feed the function,
-                        // call it with the rest of link's elements as the function's arguments
-                        try {
-                            func.apply(funcThis, link.slice(1));
-                        } catch(e) {
-                            // emit error
-                            fuze.emitter.emit('err', step, e);
-                        }
-                    } else {
+                    if (func.length !== link.length - 1) {
+                        // we should have all we need to feed the function
                         throw new Error('Number of arguments not match your function definition, wrap your code if you want to use dynamic argument list');
                     }
 
+                    if (link[link.length-1] === fuze) {
+                        // if instance of current Fuze is the last argument,
+                        // then 'next' as the callback and you probably need to handle error by you self
+                        link.pop();
+                        link.push(next);
+                    }
+                    // call it with the rest of link's elements as the function's arguments
+                    funcArgs = link.slice(1);
+                }
+                try {
+                    // call
+                    func.apply(funcThis, funcArgs);
+                } catch(e) {
+                    // emit error
+                    emit(step, e);
                 }
             }
-            next.emit = fuze.emitter.emit;// @todo any better way?
-            next.eos = function() {
-                fuze.eos.apply(next, arguments);
+            function emit(step, e) {
+                fuze.emitter.emit('err', step, e); // fuze level
+                emitter.emit('err', step, e); // chain level
             }
-            return function() {
+            next.eos = function(err) {
+                if (err)
+                    emit('err', step, err);
+                else
+                    next.apply(next, slice.call(arguments, 1));
+            }
+            var rf = function() {
                 next.apply(next, arguments);
             }
-        },
-
-        chain: function() {
-            var chains = slice.call(arguments);
-            var fuze = this;
-            function next() {
-                if (chains.length <= 0) {
-                    return;
-                }
-                var fn = chains.shift();
-                fn.apply(next, arguments);
+            rf.onError = function(callback, clear) {
+                clear && emitter.removeAllListeners('err');
+                emitter.addListener('err', callback);
+                return rf;
             }
-
-            next.eos = function() {
-                fuze.eos.apply(next, arguments);
-            }
-            return function() {
-                next.apply(next, arguments);
-            }
+            return rf;
         },
 
         times: function(time, fn, callback) {
